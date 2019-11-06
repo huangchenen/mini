d5ec60cee580bea304f5ba713a101d7c7f3e7941
huangchenen <502740730@qq.com>
2019-11-07 00:59:32 +0800

    @金亚  读一下readMe文件

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # mini
 小程序开端
 #git账号 huangchenen 5小6大7大
+
+#20191106
+@金亚  建一个mysql的数据库就可以跑起来项目了，数据库名参见配置spring-mybatis文件
--- src/main/java/com/mini/controller/UserController.java
+++ src/main/java/com/mini/controller/UserController.java
@@ -4,6 +4,8 @@ package com.mini.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.mini.entity.User;
 import com.mini.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private UserService userService;
@@ -21,6 +24,7 @@ public class UserController {
     @RequestMapping("/getUsers")
     @ResponseBody
     public String getAllUsers(){
+        logger.info("==========查询用户开始=========");
         List<User> userList = userService.getUserList();
         return JSONObject.toJSONString(userList);
     }
